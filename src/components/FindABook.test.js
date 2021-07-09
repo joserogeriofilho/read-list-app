f7ad197bd1ae53dbf74e488d07c20c335aa41464
Jose Rogerio <roger.sama@gmail.com>
2021-07-08 23:37:39 -0300

    Use async function findByAll instead of sync getAll in FindABook.test

--- src/components/FindABook.test.js
+++ src/components/FindABook.test.js
@@ -1,6 +1,7 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, } from '@testing-library/react';
 import nock from 'nock';
 import FindABook from './FindABook';
+
 
 const OPEN_LIBRARY_MOCK_RESPONSE = {
   numFound: 4,
@@ -37,11 +38,13 @@ describe('testing the FindABook component', () => {
     expect(getByPlaceholderText(/enter a book's title/i)).toBeInTheDocument();
   });
 
-  it('should render a list of books after filling the input search', () => {  
+  it('should render a list of books after filling the input search', async () => {  
     const component = render( <FindABook /> );
     const input = component.getByPlaceholderText(/enter a book's title/i);
+
     fireEvent.change(input, { target: { value: 'prometheus rising' } })
-    const itens = component.getAllByRole('listitem');
+    
+    const itens = await component.findAllByRole('listitem');
 
     expect(itens).toHaveLength(OPEN_LIBRARY_MOCK_RESPONSE.docs.length);
 
