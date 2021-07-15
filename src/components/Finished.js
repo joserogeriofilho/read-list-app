8aa43264980c1d837c109c722f57ea217bfe909d
Jose Rogerio <roger.sama@gmail.com>
2021-07-14 22:33:04 -0300

    Add Finished component feature

--- src/components/Finished.js
+++ src/components/Finished.js
@@ -1,7 +1,21 @@
+import { useSelector } from 'react-redux';
+import { selectBooks } from '../redux/bookSlice';
+
 export default function Finished() {
+  const books = useSelector(selectBooks);
+  const finishedBooks = books.filter(book => book.read);
+
   return (
     <div className={'page'}>
       <h1>Finished Books</h1>
+      { finishedBooks.length > 0 && finishedBooks.map((item, index) => (
+          <li key={index}>
+            <span>{item.title}</span>
+            <br/>
+            <span>{item.author}</span>
+          </li>
+        ))
+      }
     </div>
   )
 }
